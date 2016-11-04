be10b3ad1b1af0ddf96c956a6b92716cf52fc230
kpoon1 <kpoon@umail.ucsb.edu>
2016-11-04 12:49:39 -0700

    Created Card class, and updated Deck class to make it handle with Card instead of String

---
+++ src/edu/ucsb/cs56/projects/games/dealer/Card.java
@@ -0,0 +1,19 @@
+package edu.ucsb.cs56.projects.games.dealer;
+import java.util.*;
+import java.lang.*;
+
+public class Card{
+    private String rank;
+    private String suit;
+
+    public Card(String rank,String suit){
+	this.rank=rank;
+	this.suit=suit;
+
+    }
+
+    public String toString(){
+	return rank+" of "+suit;
+    }
+
+}
