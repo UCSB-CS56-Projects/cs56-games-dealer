a6b27ce0b099b4db660b73838e89f45acf163b76
kpoon1 <kpoon@umail.ucsb.edu>
2016-11-09 00:25:58 -0800

    kp/ex Removed everything but javadoc, modified README.md for gh-pages branch

--- src/edu/ucsb/cs56/projects/games/dealer/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The package of the actual game, contains both command line version and GUI version.
- *
- */
-
-package edu.ucsb.cs56.projects.games.dealer;
--- src/edu/ucsb/cs56/projects/games/dealer/unitTest/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * The package of the JUnit Test, to test with the game dealer.
- * It could be added more test in the future.
- *
- */
-
-package edu.ucsb.cs56.projects.games.dealer.unitTest;
