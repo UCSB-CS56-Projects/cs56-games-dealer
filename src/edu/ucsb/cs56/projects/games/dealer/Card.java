05a7d8d75f4d776633a2be86c01e85b959fc5fc9
Eric Xiao <silvershark17@csil.cs.ucsb.edu>
2016-11-20 01:11:09 -0800

    ex/kp created initial blackjack class

--- src/edu/ucsb/cs56/projects/games/dealer/Card.java
+++ src/edu/ucsb/cs56/projects/games/dealer/Card.java
@@ -22,9 +22,23 @@ public class Card {
     }
 
     /**
+     * @return the rank of the card
+     */
+    public String getRank() {
+	return this.rank;
+    }
+
+    /**
+     * @return the suit of the card
+     */
+    public String getSuit() {
+	return this.suit;
+    }
+    
+    /**
      * Overridden toString function to print this Card.
      *
-     * @retrun Returns the rank of suit of the Card
+     * @returnn Returns the rank of suit of the Card
      */
 
     @Override
